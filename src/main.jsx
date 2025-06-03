7648fb2947ae7e8bb7457b995efaaca036c25d18
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-03 09:48:20 +0630

    yaycha project setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ThemedApp from "./ThemedApp";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemedApp />
+  </StrictMode>
+);
