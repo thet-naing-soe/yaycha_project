39802086a4c8f8b3cabd753c3d094713ccdfa3f0
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-05 09:58:59 +0630

    react router dom

--- src/ThemedApp.jsx
+++ src/ThemedApp.jsx
@@ -1,12 +1,95 @@
-import { createContext, useState } from "react";
-import App from "./App.jsx";
+import { useState, createContext, useContext, useMemo } from "react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export const AppContext = createContext();
+import { deepPurple, grey } from "@mui/material/colors";
+
+import Template from "./Template";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Likes from "./pages/Likes";
+import Profile from "./pages/Profile";
+import Comments from "./pages/Comments";
+
+const AppContext = createContext();
+
+export function useApp() {
+  return useContext(AppContext);
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Template />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/comments/:id",
+        element: <Comments />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+      {
+        path: "/likes/:id",
+        element: <Likes />,
+      },
+    ],
+  },
+]);
+
 export default function ThemedApp() {
-  const [mode, setMode] = useState("dark")
+  const [showDrawer, setShowDrawer] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [globalMsg, setGlobalMsg] = useState(null);
+  const [auth, setAuth] = useState(null);
+  const [mode, setMode] = useState("dark");
+
+  const theme = useMemo(() => {
+    return createTheme({
+      palette: {
+        mode,
+        primary: deepPurple,
+        banner: mode === "dark" ? grey[800] : grey[200],
+        text: {
+          fade: grey[500],
+        },
+      },
+    });
+  }, [mode]);
+
   return (
-    <AppContext.Provider value={{ mode, setMode }}>
-      <App />
-    </AppContext.Provider>
+    <ThemeProvider theme={theme}>
+      <AppContext.Provider
+        value={{
+          showDrawer,
+          setShowDrawer,
+          showForm,
+          setShowForm,
+          globalMsg,
+          setGlobalMsg,
+          auth,
+          setAuth,
+          mode,
+          setMode,
+        }}
+      >
+        <RouterProvider router={router} />
+        <CssBaseline />
+      </AppContext.Provider>
+    </ThemeProvider>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,53 +1,29 @@
-import { useContext, useRef } from "react";
-import { AppContext } from "../ThemedApp";
-
+import { useRef } from "react";
+import { Box, TextField, Button } from "@mui/material";
 export default function Form({ add }) {
-  const {mode} = useContext(AppContext)
-  const nameRef = useRef();
   const contentRef = useRef();
-
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         const content = contentRef.current.value;
-        const name = nameRef.current.value;
-        add(content, name);
+        add(content, "Alice");
         e.currentTarget.reset();
       }}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 3,
-        padding: 10,
-        borderRadius: 8,
-        marginBottom: 20,
-        background: mode === "dark" ? "#555" : "#def",
-      }}
     >
-      <input
-        ref={contentRef}
-        type="text"
-        placeholder="Content"
-        style={{ padding: 5 }}
-      />
-      <input
-        ref={nameRef}
-        type="text"
-        placeholder="Name"
-        style={{ padding: 5 }}
-      />
-      <button
-        type="submit"
-        style={{
-          padding: 8,
-          background: "#0d6efd",
-          color: "white",
-          border: "0 none",
-        }}
-      >
-        Post
-      </button>
+      <Box sx={{ mb: 4, textAlign: "right" }}>
+        <TextField
+          inputRef={contentRef}
+          type="text"
+          placeholder="Content"
+          fullWidth
+          multiline
+          sx={{ mb: 1 }}
+        />
+        <Button variant="contained" type="submit">
+          Post
+        </Button>
+      </Box>
     </form>
   );
 }
