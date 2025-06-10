42a5303b04101dd2dc4c77c76f0ac1f6139845a4
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-10 17:41:14 +0630

    setup login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,61 @@
 import { Alert, Box, Button, TextField, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 import { useApp } from "../ThemedApp";
+import { postLogin } from "../libs/fetcher";
+import { useMutation } from "@tanstack/react-query";
 export default function Login() {
+  const usernameInput = useRef();
+  const passwordInput = useRef();
+  const [error, setError] = useState(null);
+  const handleSubmit = () => {
+    const username = usernameInput.current.value;
+    const password = passwordInput.current.value;
+    if (!username || !password) {
+      setError("username and password required");
+      return false;
+    }
+    login.mutate({ username, password });
+  };
+  const login = useMutation({
+    mutationFn: async ({ username, password }) => postLogin(username, password),
+    onError: async () => {
+      setError("Incorrect username or password");
+    },
+    onSuccess: async (result) => {
+      setAuth(result.user);
+      localStorage.setItem("token", result.token);
+      navigate("/");
+    },
+  });
   const navigate = useNavigate();
   const { setAuth } = useApp();
   return (
     <Box>
       <Typography variant="h3">Login</Typography>
-      <Alert severity="warning" sx={{ mt: 2 }}>
-        All fields required
-      </Alert>
+      {error && (
+        <Alert severity="warning" sx={{ mt: 2 }}>
+          {error}
+        </Alert>
+      )}
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          setAuth(true);
-          navigate("/");
+          handleSubmit();
         }}
       >
         <Box sx={{ display: "flex", flexDirection: "column", gap: 1, mt: 2 }}>
-          <TextField placeholder="Username" fullWidth />
-          <TextField type="password" placeholder="Password" fullWidth />
+          <TextField
+            inputRef={usernameInput}
+            placeholder="Username"
+            fullWidth
+          />
+          <TextField
+            inputRef={passwordInput}
+            type="password"
+            placeholder="Password"
+            fullWidth
+          />
           <Button type="submit" variant="contained" fullWidth>
             Login
           </Button>
